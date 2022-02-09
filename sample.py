1e5943046653f53181f2a5dbdbad2c3cc5c16c14
Jacob Rajah <47643645+JacobRajah@users.noreply.github.com>
2022-02-09 12:42:10 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,2 +1,10 @@
-def foo(a):
-  return a + 2
+"""
+  This is a function
+"""
+def increment_by_two(var_to_increment):
+    """
+    This function increments input by two
+    :param var_to_increment: param to increment
+    :return: incremented variable
+    """
+    return a + 2
